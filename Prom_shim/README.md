bd3ceff033c4d9a24bb7660b44713d40a9c8d7c1
Denny Vettom <dennysv@gmail.com>
2023-06-18 10:42:43 +0100

    Update by DV on Sun Jun 18 10:42:43 BST 2023

--- Prom_shim/README.md
+++ Prom_shim/README.md
@@ -2,3 +2,11 @@
 docker build -t flask-app .
 dk tag flask-app dennysv/flask-app
 docker push dennysv/flask-app
+
+
+# Generating requirements.txt
+python -m venv venv
+source venv/bin/activate
+pip install prometheus_client  prometheus_flask_exporter flask requests
+pip freeze > requirements.txt
+deactivate
--- Prom_shim/prom_sd.py
+++ Prom_shim/prom_sd.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
-import json
-import requests
-import time
+import json,requests,time
 from flask import Flask, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_client import Counter, Gauge, Histogram
 
-failure_counter = Counter('app_failures_total', 'Total number of failures')
+failure_counter = Counter('app_failures_total', 'Total number of failures',['app_name'])
 my_counter = Counter('my_success', 'Sample by denny',['app_name'])
-metrics = PrometheusMetrics(app=None, path='/metrics')
+metrics = PrometheusMetrics(app=None, path='/metrics', export_defaults = False)
 
 app = Flask(__name__)
 @app.route("/")
 def home_page():
-        return 'Test landing page. It worked'
+    return 'Prometheus Service Discovery Shim'
 
 @app.route('/api/v1/<appname>')
 def sd_data(appname):
@@ -26,10 +24,17 @@ def sd_data(appname):
         return "Data file not found"
 
 def fetch_data():
-    Endpoints = {"cna": 'https://vettom.github.io/api/mixed.json', "pis": 'https://vettom.github.io/api/mixed.json' }
+    # Endpoints = {"cna": 'https://vettom.github.io/api/cna.json', "pis": 'https://vettom.github.io/api/pis.json' }
+    try:
+        with open("endpoints.json", 'r') as file:
+            Endpoints = json.load(file)
+    except Exception as e:
+        print(f"Failed to load endpoints.json.", e)
+
+    print(Endpoints)
     while True:
-        for App, Link in Endpoints.items():
-            try:
+        try:
+            for App, Link in Endpoints.items():
                 # For each App Link, process URL
                 response = requests.get(Link)
                 jsonData = response.json()
@@ -45,8 +50,9 @@ def fetch_data():
                 with open(f"{App}.json", 'w') as file:
                     json.dump(promData, file)
                 print('Data retrieved successfully')
-            except Exception as e:
-                print('Error retrieving data:', e)
+        except Exception as e:
+            failure_counter.labels(App).inc()
+            print(f'Error retrieving data for {App}:', e)
         time.sleep(5)
 
 if __name__ == '__main__':
