5926e8ac9e815a30c51565277e8d42685dfe4a12
Denny Vettom <dennysv@gmail.com>
2023-06-19 09:46:14 +0100

    Works

--- Prom_shim/README.md
+++ Prom_shim/README.md
@@ -1,8 +1,18 @@
+# Summary
+Shim for custom prometheus service discovery based on domins list provided at endpoint.
+# Input
+Requires /config/endpoints.json in format below
+```json
+    {
+        // "Project or App": "Endpint URL to scrape"
+        "cna": "https://example.com/api/cna.json",
+        "pis": "https://github.io/api/pis.json"
+    }
+```
 # Building
-docker build -t flask-app .
-dk tag flask-app dennysv/flask-app
-docker push dennysv/flask-app
-
+docker build -t prom_sd_shim .
+dk tag prom_sd_shim dennysv/prom_sd_shim:0.1.3
+docker push dennysv/prom_sd_shim:0.1.3
 
 # Generating requirements.txt
 python -m venv venv
--- Prom_shim/prom_sd.py
+++ Prom_shim/prom_sd.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python3
-import json,requests,time
+import json,requests,time,os
 from flask import Flask, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
-from prometheus_client import Counter, Gauge, Histogram
+from prometheus_client import Counter, Gauge
 
-failure_counter = Counter('app_failures_total', 'Total number of failures',['app_name'])
-my_counter = Counter('my_success', 'Sample by denny',['app_name'])
+prometheus_sd_failed_configs = Gauge('prometheus_sd_failed_configs', 'SD configuration status',['app_name'])
+prometheus_sd_failed_total = Gauge('prometheus_sd_failed', 'Total failed requests',['app_name'])
 metrics = PrometheusMetrics(app=None, path='/metrics', export_defaults = False)
 
 app = Flask(__name__)
 @app.route("/")
 def home_page():
-    return 'Prometheus Service Discovery Shim'
+    return 'Prometheus Service Discovery'
 
 @app.route('/api/v1/<appname>')
 def sd_data(appname):
     try:
-        SD_File = appname + ".json"
+        SD_File = "data/" + appname + ".json"
         with open(SD_File, 'r') as file:
             data = json.load(file)
             return jsonify(data)
-    except FileNotFoundError:
-        return "Data file not found"
+    except Exception as e:
+        return "Page not found", 404
+
 
 def fetch_data():
-    # Endpoints = {"cna": 'https://vettom.github.io/api/cna.json', "pis": 'https://vettom.github.io/api/pis.json' }
-    try:
-        with open("endpoints.json", 'r') as file:
-            Endpoints = json.load(file)
-    except Exception as e:
-        print(f"Failed to load endpoints.json.", e)
-
-    print(Endpoints)
+    Endpoints = {"cna": 'https://vettom.github.io/api/cna.json', "pis": 'https://vettom.github.io/api/pis.json' }
+    # try:
+    #     with open("/config/endpoints.json", 'r') as file:
+    #         Endpoints = json.load(file)
+    # except Exception as e:
+    #     print(f"Failed to load endpoints.json.", e)
     while True:
         try:
             for App, Link in Endpoints.items():
-                # For each App Link, process URL
                 response = requests.get(Link)
                 jsonData = response.json()
                 promData = {
@@ -44,18 +42,19 @@ def fetch_data():
                                 "__meta_application": App,
                             }
                 }
-                my_counter.labels(App).inc()
-
+                prometheus_sd_failed_configs.labels(App).set(0)
                 # Write result to file
-                with open(f"{App}.json", 'w') as file:
+                with open(f"data/{App}.json", 'w') as file:
                     json.dump(promData, file)
-                print('Data retrieved successfully')
+                print(f'INFO: Data retrieved for {App}')
         except Exception as e:
-            failure_counter.labels(App).inc()
-            print(f'Error retrieving data for {App}:', e)
+            prometheus_sd_failed_configs.labels(App).set(1)
+            prometheus_sd_failed_total.labels(App).inc()
+            print(f'ERROR: Failed to retrieve data for {App}:', e)
         time.sleep(5)
-
 if __name__ == '__main__':
+    if not os.path.exists("data"):
+        os.makedirs("data")
     # Start the data retrieval in a separate thread
     import threading
     t = threading.Thread(target=fetch_data)
@@ -63,4 +62,4 @@ if __name__ == '__main__':
 
     # Start the HTTP server
     metrics.init_app(app)
-    app.run(port=8085)
+    app.run(port=8080)
